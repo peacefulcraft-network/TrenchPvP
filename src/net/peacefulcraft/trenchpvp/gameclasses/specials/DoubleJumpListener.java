94aea83cb5641bc69343a825904544377d62f0d9
18lee <18lee@LAPTOP-JURRDBB9>
2019-03-10 14:18:49 -0400

    Fixed DoubleJump. Can no longer infinitely jump

--- src/net/peacefulcraft/trenchpvp/gameclasses/specials/DoubleJumpListener.java
+++ src/net/peacefulcraft/trenchpvp/gameclasses/specials/DoubleJumpListener.java
@@ -4,6 +4,7 @@ package net.peacefulcraft.trenchpvp.gameclasses.specials;
 
 
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -14,11 +15,13 @@ import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.util.Vector;
@@ -28,7 +31,7 @@ import net.peacefulcraft.trenchpvp.gamehande.player.TrenchPlayer;
 
 public class DoubleJumpListener implements Listener
 {
-	
+	HashMap<UUID, Integer> cooldown = new HashMap<UUID, Integer>();	
 	@EventHandler
     public void onPlayerJoin(PlayerJoinEvent e){
 		Player p = e.getPlayer();
@@ -36,7 +39,28 @@ public class DoubleJumpListener implements Listener
 		p.setAllowFlight(true);
 		p.setFlying(false);
     }
-   
+	@EventHandler
+	public void resetDoubleJump(PlayerMoveEvent e) {
+		 Player p = e.getPlayer();
+	        
+		 if(p.getGameMode() == GameMode.CREATIVE || p.getGameMode() == GameMode.SPECTATOR) return;
+	        
+	     TrenchPlayer t;
+	     try {
+			t = TeamManager.findTrenchPlayer(p);
+		}catch(RuntimeException x) {
+			return;
+		}
+			
+		if(!(t.getKitType() == TrenchKits.SCOUT)) return;
+			
+		if(cooldown.containsKey(p.getUniqueId())) {	
+			if(cooldown.get(p.getUniqueId()) == 1 && p.isOnGround()) {
+				cooldown.put(p.getUniqueId(), 0);
+			}
+		}
+	}
+		
     @EventHandler
     public void onPlayerDoubleJump(PlayerToggleFlightEvent e){
         Player p = e.getPlayer();
@@ -58,19 +82,44 @@ public class DoubleJumpListener implements Listener
 				e.setCancelled(true);
 				return;
 			}
-		
-        if(p.getGameMode() != GameMode.CREATIVE){
-            e.setCancelled(true);
-            Block b = p.getWorld().getBlockAt(p.getLocation().subtract(0,2,0));
-            if(!b.getType().equals(Material.AIR)){
-       
-                Vector v = new Vector(p.getVelocity().getX(), p.getVelocity().getY(), p.getVelocity().getZ());
-                Vector forward = p.getLocation().getDirection().multiply(0.3);
-                Vector jump = p.getLocation().getDirection().multiply(0.07).setY(1);
-                v.add(forward).add(jump);
-                p.setVelocity(v);
-            	
-            }
-        }
+		if(cooldown.containsKey(p.getUniqueId())) {
+			if(cooldown.get(p.getUniqueId()) == 0) {
+				if(p.getGameMode() != GameMode.CREATIVE) {
+					//Double Jump code
+			       	e.setCancelled(true);
+				    Block b = p.getWorld().getBlockAt(p.getLocation().subtract(0,2,0));
+				    if(!b.getType().equals(Material.AIR)){
+				       
+				     	Vector v = new Vector(p.getVelocity().getX(), p.getVelocity().getY(), p.getVelocity().getZ());
+				       	Vector forward = p.getLocation().getDirection().multiply(0.3);
+				        Vector jump = p.getLocation().getDirection().multiply(0.07).setY(1);
+				        v.add(forward).add(jump);
+				        p.setVelocity(v);     
+				        
+				        cooldown.put(p.getUniqueId(), 1);
+				    }
+				}
+			} else if(cooldown.get(p.getUniqueId()) == 1){
+				p.setFlying(false);
+				e.setCancelled(true);
+				return;
+			}
+		} else {
+			if(p.getGameMode() != GameMode.CREATIVE) {
+				//Double Jump code
+		       	e.setCancelled(true);
+			    Block b = p.getWorld().getBlockAt(p.getLocation().subtract(0,2,0));
+			    if(!b.getType().equals(Material.AIR)){
+			       
+			     	Vector v = new Vector(p.getVelocity().getX(), p.getVelocity().getY(), p.getVelocity().getZ());
+			       	Vector forward = p.getLocation().getDirection().multiply(0.3);
+			        Vector jump = p.getLocation().getDirection().multiply(0.07).setY(1);
+			        v.add(forward).add(jump);
+			        p.setVelocity(v);    
+			        
+			        cooldown.put(p.getUniqueId(), 1);
+			    }
+			}
+		}
     }
 }
