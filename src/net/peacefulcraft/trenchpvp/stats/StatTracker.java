9d7d6ed2b2de296b540b2fe5cd770349803825a0
18lee <18lee@LAPTOP-JURRDBB9>
2019-04-16 10:32:19 -0400

    StatTracker outline

--- src/net/peacefulcraft/trenchpvp/stats/StatTracker.java
+++ src/net/peacefulcraft/trenchpvp/stats/StatTracker.java
@@ -1,6 +1,20 @@
 package net.peacefulcraft.trenchpvp.stats;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class StatTracker
 {
-
+	private HashMap<UUID, HashMap<String, Integer>> statMap = new HashMap<UUID, HashMap<String, Integer>>();
+	public HashMap<String, Integer> data = new HashMap<String, Integer>();
+	
+	public StatTracker() {
+		
+	}
+	
+	public void insert(HashMap<String, Integer> inner, UUID player) {
+		statMap.put(player, inner);
+	}
+	
+	
 }
