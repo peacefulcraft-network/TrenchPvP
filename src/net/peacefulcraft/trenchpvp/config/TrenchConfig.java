c9680315025eeb7b475efe1a528e8a8ee72a4ab2
parsonswy <parsonswy@gmail.com>
2019-10-14 01:02:47 -0400

    Remove Arena Context-based things from the config

--- src/net/peacefulcraft/trenchpvp/config/TrenchConfig.java
+++ src/net/peacefulcraft/trenchpvp/config/TrenchConfig.java
@@ -51,56 +51,6 @@ public class TrenchConfig {
 	public Map<String, Object> getQuit_spawn() {
 		return quit_spawn;
 	}
-
-	public void setQuit_spawn(Map<String, Object> quit_spawn) {
-		this.quit_spawn = quit_spawn;
-		c.set("trenchspawns.quit", quit_spawn);
-	}
-
-	public Map<String, Object> getRed_spawn() {
-		return red_spawn;
-	}
-
-	public void setRed_spawn(Map<String, Object> red_spawn) {
-		this.red_spawn = red_spawn;
-		c.set("trenchspawns.red", red_spawn);
-	}
-
-	public Map<String, Object> getBlue_spawn() {
-		return blue_spawn;
-	}
-
-	public void setBlue_spawn(Map<String, Object> blue_spawn) {
-		this.blue_spawn = blue_spawn;
-		c.set("trenchspawns.blue", blue_spawn);
-	}
-
-	public Map<String, Object> getRed_class_spawn() {
-		return red_class_spawn;
-	}
-
-	public void setRed_class_spawn(Map<String, Object> red_class_spawn) {
-		this.red_class_spawn = red_class_spawn;
-		c.set("trenchspawns.red_class", red_class_spawn);
-	}
-
-	public Map<String, Object> getBlue_class_spawn() {
-		return blue_class_spawn;
-	}
-
-	public void setBlue_class_spawn(Map<String, Object> blue_class_spawn) {
-		this.blue_class_spawn = blue_class_spawn;
-		c.set("trenchspawns.blue_class", blue_class_spawn);
-	}
-
-	public Map<String, Object> getSpectator_spawn() {
-		return spectator_spawn;
-	}
-
-	public void setSpectator_spawn(Map<String, Object> spectator_spawn) {
-		this.spectator_spawn = spectator_spawn;
-		c.set("trenchspawns.spectator", spectator_spawn);
-	}
 	
 	public String getDb_ip() {
 		return db_ip;
